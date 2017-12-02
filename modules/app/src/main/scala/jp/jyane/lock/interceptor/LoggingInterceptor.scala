00efe4e575979685d58a8d1c7524f9f63ba5cecf
jyane <jyane.kstm@gmail.com>
2017-12-02 23:36:37 +0900

    fix logging interceptor
    add more details

--- modules/app/src/main/scala/jp/jyane/lock/interceptor/LoggingInterceptor.scala
+++ modules/app/src/main/scala/jp/jyane/lock/interceptor/LoggingInterceptor.scala
@@ -4,29 +4,32 @@ import com.typesafe.scalalogging.StrictLogging
 import io.grpc.ForwardingServerCall.SimpleForwardingServerCall
 import io.grpc._
 
-class LoggingInterceptor extends ServerInterceptor with StrictLogging {
-  class LoggingServerCallListener[ReqT](val delegate: ServerCall.Listener[ReqT]) extends ForwardingServerCallListener[ReqT] {
-    override def onMessage(message: ReqT): Unit = {
-      logger.info(s"receive: $message")
-      super.onMessage(message)
-    }
+class LoggingServerCall[ReqT, RespT](call: ServerCall[ReqT, RespT]) extends SimpleForwardingServerCall[ReqT, RespT](call) with StrictLogging {
+  override def sendMessage(message: RespT): Unit = {
+    logger.info(s"${call.getMethodDescriptor.getFullMethodName} send data:\n$message")
+    super.sendMessage(message)
   }
 
+  override def close(status: Status, trailers: Metadata): Unit = {
+    logger.info(s"call was closed: $status, $trailers")
+    super.close(status, trailers)
+  }
+}
+
+class LoggingServerCallListener[ReqT](val delegate: ServerCall.Listener[ReqT], methodName: String) extends ForwardingServerCallListener[ReqT] with StrictLogging {
+  override def onMessage(message: ReqT): Unit = {
+    logger.info(s"$methodName receive data:\n$message")
+    super.onMessage(message)
+  }
+}
+
+class LoggingInterceptor extends ServerInterceptor with StrictLogging {
   override def interceptCall[ReqT, RespT](
     call: ServerCall[ReqT, RespT],
     headers: Metadata,
     next: ServerCallHandler[ReqT, RespT]
-  ) = {
-    new LoggingServerCallListener[ReqT](
-      next.startCall(
-        new SimpleForwardingServerCall[ReqT, RespT](call) {
-          override def sendMessage(message: RespT): Unit = {
-            logger.info(s"send data: $message")
-            super.sendMessage(message)
-          }
-        },
-        headers
-      )
-    )
+  ): LoggingServerCallListener[ReqT] = {
+    val methodName = call.getMethodDescriptor.getFullMethodName
+    new LoggingServerCallListener[ReqT](next.startCall(new LoggingServerCall(call), headers), methodName)
   }
 }
